075a91e2be33c32d2d85dfddbb2889713700cc9b
cetecom <cetecom@LCT50414>
2019-03-06 22:10:43 -0300

    Arreglos

--- index.php
+++ index.php
@@ -18,6 +18,28 @@ and open the template in the editor.
             echo "Hola ".$saludo;
             
         ?>
+        <?php
+         $meses["enero"]="verano";
+         $meses["febrero"]="verano";
+         $meses["marzo"]="verano/otoño";
+         $meses["abril"]="otoño";
+         $meses["mayo"]="otoño/invierno";
+         $meses["junio"]="invierno";
+         $meses["julio"]="invierno";
+         $meses["agosto"]="invierno";
+         $meses["septiembre"]="invierno/primavera";
+         $meses["octubre"]="primavera";
+         $meses["noviembre"]="primavera";
+         $meses["diciembre"]="primavera/verano";
+         
+         foreach ($meses as $mes){
+             echo $mes;
+         }
+         
+        ?>
+        
+        
+        
         <br>
         <pre>   <!--permite ver de forma ordena y legible-->
             Variables de Entorno        
